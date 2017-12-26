4ed24b47fc144f5f9f7511017617032b520fcc7d
richardjohns <richard.johns193@gmail.com>
2017-12-27 01:27:40 +1300

    updates for articles index and links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,30 @@
 class ArticlesController < ApplicationController
+  # A frequent practice is to place the standard CRUD actions in each controller in the following order: index, show, new, edit, create, update and destroy. You may use any order you choose, but keep in mind that these are public methods; as mentioned earlier in this guide, they must be placed before declaring private visibility in the controller.
+  def index
+    @articles = Article.all
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def new
+    @article = Article.new
   end
 
   def create
-  render plain: params[:article].inspect
-
+  # render plain: params[:article].inspect
+  # @article = Article.new(params.require(:article).permit(:title, :text)) 
+    @article = Article.new(article_params)
+    if @article.save
+    redirect_to @article
+    else
+    render 'new'
+    end
+  end
+ 
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
