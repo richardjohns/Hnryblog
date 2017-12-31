010d6698a9be900435a33294553256870dc1a863
richardjohns <richard.johns193@gmail.com>
2017-12-31 14:59:36 +1300

    vue import now working in application view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,10 +10,10 @@
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 
-document.addEventListener("turbolinks:load", () => {
+document.addEventListener("turbolinks:load", function() {
     var element = document.querySelector("#boards")
     if (element != undefined) {
-        const App = new Vue({
+        const app = new Vue({
             el: element,
             data: {
                 lists: JSON.parse(element.dataset.lists)
